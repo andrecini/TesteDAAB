b8071504e2f19a34f8707ad584921872b88d55ae
andrepresys <andre.presys@gmail.com>
2021-02-25 14:22:46 -0300

    Teste Inicial

---
+++ Form1.Designer.vb
@@ -0,0 +1,223 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.txtCodigo = New System.Windows.Forms.TextBox()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.txtDescricao = New System.Windows.Forms.TextBox()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.txtVenda = New System.Windows.Forms.MaskedTextBox()
+        Me.txtCusto = New System.Windows.Forms.MaskedTextBox()
+        Me.PictureBox1 = New System.Windows.Forms.PictureBox()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.btnSair = New System.Windows.Forms.Button()
+        Me.btnAdd = New System.Windows.Forms.Button()
+        Me.btnAtualizar = New System.Windows.Forms.Button()
+        Me.btnRemove = New System.Windows.Forms.Button()
+        Me.btnNewDS = New System.Windows.Forms.Button()
+        CType(Me.PictureBox1,System.ComponentModel.ISupportInitialize).BeginInit
+        Me.SuspendLayout
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = true
+        Me.Label1.Font = New System.Drawing.Font("Microsoft Sans Serif", 10!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0,Byte))
+        Me.Label1.Location = New System.Drawing.Point(27, 23)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(63, 17)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "Código:"
+        '
+        'txtCodigo
+        '
+        Me.txtCodigo.Font = New System.Drawing.Font("Microsoft Sans Serif", 10!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0,Byte))
+        Me.txtCodigo.Location = New System.Drawing.Point(96, 20)
+        Me.txtCodigo.Name = "txtCodigo"
+        Me.txtCodigo.Size = New System.Drawing.Size(139, 23)
+        Me.txtCodigo.TabIndex = 1
+        Me.txtCodigo.Text = "6"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = true
+        Me.Label2.Font = New System.Drawing.Font("Microsoft Sans Serif", 10!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0,Byte))
+        Me.Label2.Location = New System.Drawing.Point(27, 70)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(85, 17)
+        Me.Label2.TabIndex = 2
+        Me.Label2.Text = "Descrição:"
+        '
+        'txtDescricao
+        '
+        Me.txtDescricao.Font = New System.Drawing.Font("Microsoft Sans Serif", 10!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0,Byte))
+        Me.txtDescricao.Location = New System.Drawing.Point(30, 99)
+        Me.txtDescricao.Multiline = true
+        Me.txtDescricao.Name = "txtDescricao"
+        Me.txtDescricao.Size = New System.Drawing.Size(346, 83)
+        Me.txtDescricao.TabIndex = 3
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = true
+        Me.Label3.Font = New System.Drawing.Font("Microsoft Sans Serif", 10!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0,Byte))
+        Me.Label3.Location = New System.Drawing.Point(26, 204)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(129, 17)
+        Me.Label3.TabIndex = 4
+        Me.Label3.Text = "Preço de Venda:"
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = true
+        Me.Label4.Font = New System.Drawing.Font("Microsoft Sans Serif", 10!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0,Byte))
+        Me.Label4.Location = New System.Drawing.Point(26, 265)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(124, 17)
+        Me.Label4.TabIndex = 5
+        Me.Label4.Text = "Preço de Custo:"
+        '
+        'txtVenda
+        '
+        Me.txtVenda.Font = New System.Drawing.Font("Microsoft Sans Serif", 10!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0,Byte))
+        Me.txtVenda.Location = New System.Drawing.Point(161, 201)
+        Me.txtVenda.Name = "txtVenda"
+        Me.txtVenda.Size = New System.Drawing.Size(108, 23)
+        Me.txtVenda.TabIndex = 6
+        '
+        'txtCusto
+        '
+        Me.txtCusto.Font = New System.Drawing.Font("Microsoft Sans Serif", 10!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0,Byte))
+        Me.txtCusto.Location = New System.Drawing.Point(161, 259)
+        Me.txtCusto.Name = "txtCusto"
+        Me.txtCusto.Size = New System.Drawing.Size(108, 23)
+        Me.txtCusto.TabIndex = 7
+        '
+        'PictureBox1
+        '
+        Me.PictureBox1.Location = New System.Drawing.Point(305, 194)
+        Me.PictureBox1.Name = "PictureBox1"
+        Me.PictureBox1.Size = New System.Drawing.Size(71, 67)
+        Me.PictureBox1.TabIndex = 8
+        Me.PictureBox1.TabStop = false
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(256, 11)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(57, 23)
+        Me.Button1.TabIndex = 9
+        Me.Button1.Text = "Localizar"
+        Me.Button1.UseVisualStyleBackColor = true
+        '
+        'btnSair
+        '
+        Me.btnSair.Location = New System.Drawing.Point(305, 267)
+        Me.btnSair.Name = "btnSair"
+        Me.btnSair.Size = New System.Drawing.Size(71, 23)
+        Me.btnSair.TabIndex = 10
+        Me.btnSair.Text = "Sair"
+        Me.btnSair.UseVisualStyleBackColor = true
+        '
+        'btnAdd
+        '
+        Me.btnAdd.Location = New System.Drawing.Point(256, 40)
+        Me.btnAdd.Name = "btnAdd"
+        Me.btnAdd.Size = New System.Drawing.Size(57, 23)
+        Me.btnAdd.TabIndex = 11
+        Me.btnAdd.Text = "Adicionar"
+        Me.btnAdd.UseVisualStyleBackColor = true
+        '
+        'btnAtualizar
+        '
+        Me.btnAtualizar.Location = New System.Drawing.Point(319, 11)
+        Me.btnAtualizar.Name = "btnAtualizar"
+        Me.btnAtualizar.Size = New System.Drawing.Size(57, 23)
+        Me.btnAtualizar.TabIndex = 12
+        Me.btnAtualizar.Text = "Atualizar"
+        Me.btnAtualizar.UseVisualStyleBackColor = true
+        '
+        'btnRemove
+        '
+        Me.btnRemove.Location = New System.Drawing.Point(319, 40)
+        Me.btnRemove.Name = "btnRemove"
+        Me.btnRemove.Size = New System.Drawing.Size(57, 23)
+        Me.btnRemove.TabIndex = 13
+        Me.btnRemove.Text = "Remover"
+        Me.btnRemove.UseVisualStyleBackColor = true
+        '
+        'btnNewDS
+        '
+        Me.btnNewDS.Location = New System.Drawing.Point(256, 69)
+        Me.btnNewDS.Name = "btnNewDS"
+        Me.btnNewDS.Size = New System.Drawing.Size(120, 23)
+        Me.btnNewDS.TabIndex = 14
+        Me.btnNewDS.Text = "Criar DataSet"
+        Me.btnNewDS.UseVisualStyleBackColor = true
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6!, 13!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.Color.FromArgb(CType(CType(202,Byte),Integer), CType(CType(249,Byte),Integer), CType(CType(249,Byte),Integer))
+        Me.ClientSize = New System.Drawing.Size(406, 302)
+        Me.Controls.Add(Me.btnNewDS)
+        Me.Controls.Add(Me.btnRemove)
+        Me.Controls.Add(Me.btnAtualizar)
+        Me.Controls.Add(Me.btnAdd)
+        Me.Controls.Add(Me.btnSair)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.PictureBox1)
+        Me.Controls.Add(Me.txtCusto)
+        Me.Controls.Add(Me.txtVenda)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.txtDescricao)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.txtCodigo)
+        Me.Controls.Add(Me.Label1)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        CType(Me.PictureBox1,System.ComponentModel.ISupportInitialize).EndInit
+        Me.ResumeLayout(false)
+        Me.PerformLayout
+
+End Sub
+
+    Friend WithEvents Label1 As Label
+    Friend WithEvents txtCodigo As TextBox
+    Friend WithEvents Label2 As Label
+    Friend WithEvents txtDescricao As TextBox
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Label4 As Label
+    Friend WithEvents txtVenda As MaskedTextBox
+    Friend WithEvents txtCusto As MaskedTextBox
+    Friend WithEvents PictureBox1 As PictureBox
+    Friend WithEvents Button1 As Button
+    Friend WithEvents btnSair As Button
+    Friend WithEvents btnAdd As Button
+    Friend WithEvents btnAtualizar As Button
+    Friend WithEvents btnRemove As Button
+    Friend WithEvents btnNewDS As Button
+End Class
---
+++ Form1.vb
@@ -0,0 +1,175 @@
+﻿Imports System.Data.SqlClient
+Imports System.Text
+
+Public Class Form1 
+
+    Dim connString As String
+    Public conexaoAccess As IDbConnection 
+    Public dr As SqlDataReader
+    Public Dim Daab As ApplicationBlocks.Data.SqlDbHelper = New ApplicationBlocks.Data.SqlDbHelper()
+    Public Dim cnString As String = "Data Source=DB2012\SQLEXPRESS2008R2;Initial Catalog=@SQL_inVB;Persist Security Info=True;User ID=sa;Password=admin"
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        '''connString = "Data Source=DB2012\SQLEXPRESS2008R2;Initial Catalog=@SQL_inVB;Persist Security Info=True;User ID=sa;Password=admin"
+        '''conexaoAccess.ConnectionString = connString
+
+        TesteConnection()
+    End Sub
+
+    #Region "Botões"
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        conexaoAccess.Open()
+        '''Dim str As String
+        '''str = "SELECT * FROM Produtos WHERE (Codigo = " & Convert.ToInt32(txtCodigo.Text) & ")"
+        ''' Dim cmd As SqlCommand = New SqlCommand(str, conexaoAccess)
+        '''dr = cmd.ExecuteReader
+        '''
+        
+        TesteSelect()
+
+        While dr.Read()
+            Try
+            txtDescricao.Text = dr("Descricao").ToString
+            txtCusto.Text = String.Format("{0:C2}", dr("Custo"))
+            txtVenda.Text = String.Format("{0:C2}", dr("Venda"))
+            Catch
+                MessageBox.Show("ERROR!!!!!!!!!!!!!!!!!!!!!!!!!")
+            End Try
+        End While
+        conexaoAccess.Close()
+    End Sub
+
+    Private Sub btnSair_Click(sender As Object, e As EventArgs) Handles btnSair.Click
+        Me.Close()
+    End Sub
+
+    Private Sub btnAdd_Click(sender As Object, e As EventArgs) Handles btnAdd.Click
+        conexaoAccess.Open()
+
+        TesteInsert()
+
+        PadraoInicial()
+
+        conexaoAccess.Close()
+    End Sub
+
+    Private Sub btnAtualizar_Click(sender As Object, e As EventArgs) Handles btnAtualizar.Click
+        conexaoAccess.Open()
+
+        TesteUpdate()
+
+        PadraoInicial()
+
+        conexaoAccess.Close()
+    End Sub
+
+    Private Sub btnRemove_Click(sender As Object, e As EventArgs) Handles btnRemove.Click
+        conexaoAccess.Open()
+
+        TesteRemove()
+
+        PadraoInicial()
+
+        conexaoAccess.Close()
+    End Sub
+
+    Private Sub btnNewDS_Click(sender As Object, e As EventArgs) Handles btnNewDS.Click
+        conexaoAccess.Open()
+
+        TesteDataSet()
+
+        PadraoInicial()
+
+        conexaoAccess.Close()
+    End Sub
+    #End Region
+
+    #Region "Testes DAAB"
+    Private Sub TesteConnection()
+        
+        conexaoAccess = Daab.NewConnection(cnString)
+    End Sub
+
+    Private Sub TesteSelect() 
+        Dim sql As New StringBuilder
+       
+        sql.AppendLine("SELECT * FROM Produtos")
+        sql.AppendLine("WHERE Codigo = @Codigo")
+        Dim param = Daab.NewParameter("@Codigo", Convert.ToInt32(txtCodigo.Text))
+        
+    End Sub
+
+    Private Sub TesteInsert()
+        Dim sql As New StringBuilder
+        sql.AppendLine("INSERT INTO Produtos VALUES ")
+        sql.AppendLine("(@Codigo, @Descricao, @Custo, @Venda)")
+        
+        Dim paramContact(3) As IDataParameter
+
+        paramContact(0) = Daab.NewParameter("@Codigo", DbType.Int32)
+        paramContact(0).Value = Convert.ToInt32(txtCodigo.Text)
+
+        paramContact(1) = Daab.NewParameter("@Descricao", DbType.AnsiString)
+        paramContact(1).Value = txtDescricao.Text
+        
+        paramContact(2) = Daab.NewParameter("@Custo", DbType.Double)
+        paramContact(2).Value = Convert.ToDouble(txtCusto.Text)
+
+        paramContact(3) = Daab.NewParameter("@Venda", DbType.Double)
+        paramContact(3).Value = Convert.ToDouble(txtVenda.Text)
+
+        Daab.ExecuteNonQuery(cnString, CommandType.Text, sql.ToString(), paramContact)
+
+    End Sub
+
+    Private Sub TesteUpdate()
+        Dim sql As New StringBuilder
+        sql.AppendLine("UPDATE Produtos SET Venda = @Venda, Custo = @Custo WHERE (Codigo = @Codigo)")
+        
+        Dim paramContact(2) As IDataParameter
+
+        paramContact(0) = Daab.NewParameter("@Venda", DbType.Double)
+        paramContact(0).Value = Convert.ToDouble(txtVenda.Text)
+
+        paramContact(1) = Daab.NewParameter("@Custo", DbType.Double)
+        paramContact(1).Value = Convert.ToDouble(txtCusto.Text)
+
+        paramContact(2) = Daab.NewParameter("@Codigo", DbType.Int32)
+        paramContact(2).Value = Convert.ToInt32(txtCodigo.Text)
+
+        Daab.ExecuteNonQuery(cnString, CommandType.Text, sql.ToString(), paramContact)
+    End Sub
+
+    Private Sub TesteRemove()
+        Dim sql As New StringBuilder
+        sql.AppendLine("DELETE FROM Produtos")
+        sql.AppendLine("WHERE Codigo = @Codigo")
+        Dim param = Daab.NewParameter("@Codigo", Convert.ToInt32(txtCodigo.Text))
+        dr = Daab.ExecuteReader(cnString, CommandType.Text, sql.ToString(), param)
+    End Sub
+
+    Private Sub TesteDataSet()
+        Dim ds As DataSet
+        Dim sql As New StringBuilder
+        Dim testeDs As Int16 = 0
+
+        sql.AppendLine("SELECT * FROM Produtos")
+        sql.AppendLine("WHERE Codigo = @Codigo")
+        Dim param = Daab.NewParameter("@Codigo", Convert.ToInt32(txtCodigo.Text))
+        ds = Daab.ExecuteDataset(cnString, CommandType.Text, sql.ToString(), param)
+
+        testeDs += 1 
+
+        Console.WriteLine(testeDs)
+    End Sub
+
+#End Region
+
+    Private Sub PadraoInicial()
+        txtCodigo.Text = ""
+        txtCusto.Text = ""
+        txtDescricao.Text = ""
+        txtVenda.Text = ""
+    End Sub
+
+End Class
